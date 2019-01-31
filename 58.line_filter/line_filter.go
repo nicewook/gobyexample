7eda2018c594293104a57b78e1f9f750879e0b0d
Jeong Hyunseok <nicewook@hotmail.com>
2019-01-31 09:00:27 +0900

    done to 58_line_filter

---
+++ 58.line_filter/line_filter.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	num := 0
+	for scanner.Scan() {
+		num++
+		ucl := strings.ToUpper(scanner.Text())
+		fmt.Println(num, ucl)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "error:", err)
+		os.Exit(1)
+	}
+}
